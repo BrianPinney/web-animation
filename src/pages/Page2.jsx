3d9879acc815208504f6a89ddf2e1e55d98196ab
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-10 17:39:36 -0400

    more css animation.

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,11 +1,31 @@
-
+import { Col, Container, Row } from "react-bootstrap";
+import "../styles/page2.css"
 
 export default function Page2(){
 
+const imgRoot = process.env.PUBLIC_URL + "/images/"
+
     return (
-       <> 
-        <h2>Page 2</h2>
+       <section> 
+        <Container>
+            <Row>
+                <Col><button class='pg2-button button1'>X axis</button></Col>
+                <Col><button class='pg2-button button2'>Y axis</button></Col>
+                <Col><button class='pg2-button button3'>Scale</button></Col>
+            </Row>
+            <Row>
+                <Col>
+                <div className="profile">
+                    <img src={imgRoot+"pict1.webp"} alt="profile" />
+                    <div className="Profile-text">
+                        <h3>Bunny Paridise</h3>
+                        <a href="#" class="profile-button" rel="noreferrer">Buy Now!</a>
+                    </div>
+                    </div>
+                    </Col>
+            </Row>
+        </Container>
         
-        </>
+        </section>
     )
 }
